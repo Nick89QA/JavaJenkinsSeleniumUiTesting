fe2743f29ea0c2f43f8057dd155f0a3198e60d69
Nickolay Petrych <npetrych@alfabank.ru>
2024-05-12 16:01:45 +0100

    create pageFactory

---
+++ src/test/java/LoginPage.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BaseTest {
+
+
+    //WebElement userEmail = driver.findElement(By.id("userEmail"));
+    @FindBy(id="userEmail")
+    WebElement userEmail;
+
+    @FindBy(id=)
+
+
+}
