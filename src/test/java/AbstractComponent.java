5992df90d7819b7793b1134657e56f3ae53d81ad
Nickolay Petrych <npetrych@alfabank.ru>
2024-05-16 21:31:37 +0100

    add a new functions in ProductCatalogue 2

--- src/test/java/AbstractComponent.java
+++ src/test/java/AbstractComponent.java
@@ -1,22 +1,26 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class AbstractComponent {
+    static WebDriver driver;
 
-    WebDriver driver;
-
+    private static final WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     public AbstractComponent(WebDriver driver) {
         this.driver = driver;
 
     }
 
     public void waitForElementToAppear(By findBy) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOfElementLocated(findBy));
     }
 
+    public void waitForElementToDisappear(WebElement ele) {
+        wait.until(ExpectedConditions.visibilityOf(ele));
+
+    }
 }
--- src/test/java/StandAloneTest.java
+++ src/test/java/StandAloneTest.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class
 StandAloneTest extends BaseTest {
 
+    String productName = "ZARA COAT 3";
+
     @Test
     public void loginAndFindElementInList() {
 
@@ -16,11 +18,12 @@ StandAloneTest extends BaseTest {
         loginPage.loginApplication("anshika@gmail.com", "Iamking@000");
         ProductCatalogue productCatalogue = new ProductCatalogue(driver);
         List<WebElement> products = productCatalogue.getProductList();
+         productCatalogue.addProductToCart(productName);
 
 
-        WebElement prod = (WebElement) products.stream().filter(product -> product.
-                findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
-        prod.findElement(By.cssSelector("//button[@class='btn w-10 rounded' and contains(text(), 'Add To Cart')]")).click();
+//        WebElement prod = (WebElement) products.stream().filter(product -> product.
+//                findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+//        prod.findElement(By.cssSelector("//button[@class='btn w-10 rounded' and contains(text(), 'Add To Cart')]")).click();
 
     }
 
