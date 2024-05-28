4dc0ab406d40a5ce298ab7934683c26e9e591570
Nickolay Petrych <npetrych@alfabank.ru>
2024-05-28 21:32:12 +0100

    add new functions

--- src/test/java/MainPage.java
+++ src/test/java/MainPage.java
@@ -7,16 +7,17 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 
 /**
- * Go to the main Page of BrowserStack and search word "Selenium" and get count of result
+ * Go to the main Page of BrowserStack and search word "Selenium" and get current count of result
  */
 
 
 public class MainPage extends BaseSeleniumPage {
 
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     private String expectedSearchResultPageUrl = "https://www.browserstack.com/search?query=selenium&type=all";
     private String currentPageUrl = driver.getCurrentUrl();
 
@@ -33,8 +34,8 @@ public class MainPage extends BaseSeleniumPage {
     @FindBy(xpath = "//input[@type='search']")
     WebElement searchFieldElement;
 
-    @FindBy(xpath = "//span[@class='ais-Stats-text']//strong[contains(text(),'1439 Results')]")
-    WebElement searchResultCount;
+    @FindBy(xpath = "//span[@class='ais-Stats-text']")
+    WebElement searchResultContainer;
 
 
     public void goToMainPageUrl() {
@@ -44,26 +45,24 @@ public class MainPage extends BaseSeleniumPage {
 
     public void clickAndSearchSeleniumInSearchField() {
         searchButtonElement.click();
-        //wait.until(ExpectedConditions.elementToBeClickable(searchButtonElement)).click();
         searchFieldElement.sendKeys("Selenium");
         searchFieldElement.sendKeys(Keys.ENTER);
+        wait.until(ExpectedConditions.visibilityOf(searchResultContainer));
+
+        WebElement searchResultCounterElement = searchResultContainer.findElement(By.tagName("strong"));
+        if (searchResultCounterElement.isDisplayed()) {
+            String searchResultCount = searchResultCounterElement.getText();
+            System.out.println("Count is " + searchResultCount);
+        } else {
+            System.out.println("Search result count element not found");
+        }
+        wait.until(ExpectedConditions.urlToBe(expectedSearchResultPageUrl));
         if (expectedSearchResultPageUrl.equals(currentPageUrl)) {
             System.out.println("You are on the target page");
         } else {
             System.out.println("You are not on the target page");
         }
 
-//     //   wait.until(ExpectedConditions.presenceOfElementLocated(By.id("all-ds-stats")));
-//      //  wait.until(ExpectedConditions.visibilityOf(searchResultCount));
-//
-//        try {
-//           wait.until(ExpectedConditions.presenceOfElementLocated(By));
-//
-//
-//            System.out.println("--Successfully reached search result page with count--");
-//        } catch (TimeoutException e) {
-//            System.out.println("---Failed to find search result count within wait time--");
-//        }
-//    }
     }
+
 }
